a2d2cabe89247fc4c34fbc8c3fc7d8e012d8473f
johnholcomb10 <johntholcomb10@gmail.com>
2025-03-24 15:35:45 -0500

    Pause Menu
    Allows player to pause using escape key. Menu shows controls and allows to continue game, go to the main menu, or quit.

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverPanel;
+    public GameObject pauseGamePanel;
     private SpriteMovement player;
+    public static bool isPaused = false;
     public void Start()
     {
         gameOverPanel.gameObject.SetActive(false);
+        pauseGamePanel.gameObject.SetActive(false);
     }
 
      public void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
     }
 
     public void GameOver() 
@@ -25,5 +38,30 @@ public class GameManager : MonoBehaviour
     {
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
+    }
+
+    public void PauseGame()
+    {
+        pauseGamePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseGamePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 0f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
