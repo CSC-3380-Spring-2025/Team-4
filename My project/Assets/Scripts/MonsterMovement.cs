270e2cef2373fa4cc5e10cd80de70b28bccdb223
jdugan415 <jdugan4@lsu.edu>
2025-04-11 15:22:04 -0500

    I hate unity

--- My project/Assets/Scripts/MonsterMovement.cs
+++ My project/Assets/Scripts/MonsterMovement.cs
@@ -3,37 +3,29 @@ using UnityEngine;
 public class MonsterMovement : MonoBehaviour
 {
     public Transform[] patrolPoints;
-    public float moveSpeed;
-    public int patrolDestination;
+    public float moveSpeed = 2f;
+    private int patrolDestination = 0;
     private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        // Get the SpriteRenderer component
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-
+        if (spriteRenderer == null)
+        Debug.LogWarning(name + " is missing a SpriteRenderer in children!");
     }
+
     void Update()
     {
-        if(patrolDestination == 0)
+        Transform targetPoint = patrolPoints[patrolDestination];
+        transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
+
+        float distance = Vector2.Distance((Vector2)transform.position, (Vector2)targetPoint.position);
+        if (distance < 0.5f)
         {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f)
-            {
-                //transform.localScale = new Vector2(-1, 1);
-                patrolDestination = 1;
-            }
+            patrolDestination = (patrolDestination + 1) % patrolPoints.Length;
         }
-        if(patrolDestination == 1)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
-            {
-                //transform.localScale = new Vector2(1, 1);
-                patrolDestination = 0;
-            }
-        }
-        Vector2 direction = patrolPoints[patrolDestination].position - transform.position;
+
+        Vector2 direction = targetPoint.position - transform.position;
         if (direction.x != 0)
         {
             spriteRenderer.flipX = direction.x > 0;
